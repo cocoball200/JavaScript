73def21bfc7ec72f6cdb3ba850b1c8ddb4902c65
Mihyeon Byeon <cocoball200@gmail.com>
2020-09-30 17:10:27 +0900

    todo list생성, foreach 사용

--- todo.js
+++ todo.js
@@ -4,15 +4,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function printToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  //   control + command +space  to add emoji
+
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 // 어떻게 계속 추가되는지 확인하기.
 function handleSubmit(event) {
@@ -22,10 +38,16 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    console.log(parsedToDos);
+    parsedToDos.forEach(function (toDo) {
+      printToDo(toDo.text);
+    });
   }
 }
+
 function init() {
   loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
