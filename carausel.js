35177298ff7e7758bad757fd9445b0a6e3829578
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-04 20:35:22 +0900

    index찾기

---
+++ carausel.js
@@ -0,0 +1,17 @@
+const chevron = document.querySelector(".chevron");
+const img = document.querySelectorAll("img");
+//list형으로 반환.
+console.log(img);
+
+function nextpage(event) {
+  console.log(img.indexof());
+  var num = img.indexo;
+}
+
+function init() {
+  if (chevron.addEventListener("click", nextpage)) {
+  } else {
+  }
+}
+
+init();
