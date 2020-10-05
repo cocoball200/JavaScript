fff4366e4bb4894c0b218203e939c553973f3d40
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-05 21:18:10 +0900

    add location

--- carausel.js
+++ carausel.js
@@ -1,17 +1,29 @@
-const chevron = document.querySelector(".chevron");
+const chevron1 = document.querySelector(".chevron1");
+const chevron2 = document.querySelector(".chevron2");
 const img = document.querySelectorAll("img");
+const plus = 1
+
 //list형으로 반환.
 console.log(img);
 
 function nextpage(event) {
-  console.log(img.indexof());
-  var num = img.indexo;
+  img[0].innerHTML(hidden);
 }
 
-function init() {
-  if (chevron.addEventListener("click", nextpage)) {
-  } else {
+function add(){
+  
+
+}
+
+location.href="";
+
+function init(){
+  if(chevron1.addEventListener("click",add){
+
+  }else{
+
   }
-}
 
+}
 init();
+
