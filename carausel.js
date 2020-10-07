53cfe7718595bfb6693eadacce03872d84ee25f1
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-07 12:55:48 +0900

    study position,sliding

--- carausel.js
+++ carausel.js
@@ -1,6 +1,6 @@
 const imgList = document.querySelectorAll(".images");
 const imgLength = imgList.length;
-console.log(imgLength);
+const box = document.querySelector(".box__column_center");
 
 var imgIdx = 0;
 var chevron = document.querySelectorAll(".chevron");
@@ -23,8 +23,10 @@ function miner(num) {
   showImg(imgIdx);
 }
 function showImg(imgIdx) {
-  if ((imgIdx = -1)) {
-    imgIdx = imgLength - 1; // inx는 0부터 시작, 0~4
+  if (imgIdx == -1) {
+    imgIdx == imgLength - 1; // idx는 0부터 시작, 0~4
+    //!! 여기서 어떻게 스타일 줘서 css 변화시켜야할지 모르겠어요.
+    box[imgIdx].Style.Display;
   } else {
   }
 }
