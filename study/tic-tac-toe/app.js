c82b5d3f2d56ae0f553deafef2425253989d8ae0
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-15 22:05:00 +0900

    add target

--- study/tic-tac-toe/app.js
+++ study/tic-tac-toe/app.js
@@ -4,25 +4,43 @@ const restDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
 
 //game constants 
-const xSymbol = 'x';
-const oSymbol = 'o';
+const xSymbol = 'X';
+const oSymbol = 'O';
+let player = xSymbol;
 
 //game variables
 let gameIsLive = true;
 let xIsNext = true;
+let scoreArray = ['', '', '', '', '', '', '', '', ''];
 
+function addScore(clickTarget, targetIndex) {
+    scoreArray[targetIndex] = player;
+    clickTarget.innerHTML = player;
+    player = player == xSymbol ? oSymbol : xSymbol;
 
-
-const handleCellClick = (e) => {
-    const classList = e.target.classList;
-
-    if (xIsNext) {
-        // classList.add('x');
-    } else {
-        // classList.add('o');
-    }
 
 }
-for (const cellDiv of cellDivs) {
-    cellDiv.addEventListener('click', handleCellClick)
+
+function startGame(event) {
+    var clickTarget = event.target;
+    var targetIndex = parseInt(clickTarget.getAttribute('index'));
+    addScore(clickTarget, targetIndex);
 }
+
+function makingClassName() {
+    cellDivs.forEach(function (item, index) {
+        item.addEventListener("click", startGame);
+
+    })
+
+
+}
+
+function init() {
+
+    makingClassName();
+
+
+}
+
+init();
