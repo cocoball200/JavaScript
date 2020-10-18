1eed62d82b152ba31564be4fe264d9ddac202913
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-18 22:56:54 +0900

    update name, clickedPart

--- study/tic-tac-toe/app.js
+++ study/tic-tac-toe/app.js
@@ -2,45 +2,33 @@
 const statusDiv = document.querySelector('.status');
 const restDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
+const gameGridDivs = document.querySelector('.game-grid');
+
+
 
 //game constants 
 const xSymbol = 'X';
 const oSymbol = 'O';
-let player = xSymbol;
+const CLICK_EVENT = 'click';
 
 //game variables
+let player = xSymbol;
 let gameIsLive = true;
 let xIsNext = true;
 let scoreArray = [];
 
-function addScore(clickTarget, targetIndex) {
+function handleScore(clickTarget, targetIndex) {
     scoreArray[targetIndex] = player;
-    clickTarget.innerHTML = player;
+    clickTarget.textContent = player;
     player = player == xSymbol ? oSymbol : xSymbol;
-
-
 }
 
 function startGame(event) {
     var clickTarget = event.target;
     var targetIndex = parseInt(clickTarget.getAttribute('index'));
-    addScore(clickTarget, targetIndex);
+    handleScore(clickTarget, targetIndex);
 }
 
-function makingClassName() {
-    cellDivs.forEach(function (item, index) {
-        item.addEventListener("click", startGame);
-
-    })
+gameGridDivs.addEventListener(CLICK_EVENT, startGame);
 
 
-}
-
-function init() {
-
-    makingClassName();
-
-
-}
-
-init();
