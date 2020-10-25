d5f4094b6ff0e6d9ed29b9107dd4d2f457d00427
Mihyeon Byeon <cocoball200@gmail.com>
2020-10-25 23:20:55 +0900

    update index.html

--- study/quiz/app.js
+++ study/quiz/app.js
@@ -1,11 +1,3 @@
-const wholeContainer = document.querySelector('.container__whole');
-const startButton = document.querySelector('.start__button');
-const submitButton = document.querySelector('.submit__button');
-const nextButton = document.querySelector('.next__button');
-const questionTitle = document.querySelector('.title__text');
-const codeTEXT = document.querySelector('.code__text');
-const choiceDIv = document.querySelector('.container__choice');
-const choiceText = document.querySelector('.choice__text'); //list
 const CLICK_EVENT = "click";
 const HIDDEN = "hidden";
 
@@ -40,66 +32,3 @@ const loadedData = [
     }
 ];
 
-var submitedAnswer = 99;
-var count = 0;
-
-function showChoices(lengths, choiceData) {
-    var divList = [];
-    for (let choiceIndex = 0; choiceIndex < lengths; choiceIndex++) {
-        var div = document.createElement("div");
-        div.classList.add("choice__text");
-        div.textContent = choiceData[choiceIndex];
-        div.setAttribute("data-index", choiceIndex);
-        divList.push(choiceDIv.appendChild(div));
-
-    }
-    return divList;
-}
-
-function handlerChoiceClick(event) {
-    if (!null) {
-        var userChoice = event.target;
-        submitedAnswer = parseInt(userChoice.getAttribute('data-index'), 4);
-    }
-}
-
-function showQuestion() {
-    for (var index in loadedData) {
-        console.log(count);
-        questionTitle.textContent = loadedData[index].question;
-        if (!null) {
-            codeTEXT.textContent = loadedData[index].code;
-        } //if
-        showChoices(loadedData[index].choices.length, loadedData[index].choices);
-        console.log(index);
-        choiceDIv.addEventListener(CLICK_EVENT, handlerChoiceClick);
-        submitButton.addEventListener(CLICK_EVENT, function () {
-            var correctAnswer = loadedData[index].correctAnswer;
-            console.log(correctAnswer, submitedAnswer);
-            console.log(submitedAnswer === correctAnswer);
-            if (submitedAnswer === correctAnswer) {
-                submitButton.setAttribute(HIDDEN, "");
-                nextButton.removeAttribute(HIDDEN);
-            }
-        }); //addevent
-        nextButton.addEventListener(CLICK_EVENT, function () {
-        })
-    }//for
-}//function
-
-function startGame() {
-    wholeContainer.removeAttribute(HIDDEN);
-    startButton.setAttribute(HIDDEN, "");
-    submitButton.removeAttribute(HIDDEN;
-    showQuestion();
-}
-
-function init() {
-    wholeContainer.setAttribute(HIDDEN, "");
-    submitButton.setAttribute(HIDDEN, "");
-    nextButton.setAttribute(HIDDEN, "");
-    startButton.removeAttribute(HIDDEN)
-    startButton.addEventListener(CLICK_EVENT, startGame);
-}
-
-init();
