9ecf9bdc91f1e5209c34c0b8b47f7e4c38ef420e
Mihyeon Byeon <cocoball200@gmail.com>
2020-11-07 22:46:29 +0900

    Update prototype.js

--- study/prototype.js
+++ study/prototype.js
@@ -1,12 +1,23 @@
-var Student = function (name) {
-    this._name = name;
+var Person = function (name, age, sex, number) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+    this.number = number;
+}
+
+var Student = function (studentNumber) {
+    this.studentNumber = studentNumber;
 };
 
-Student.prototype.getName = function () {
-    return this._name;
+Person.prototype.getName = function () {
+    return this.name;
 }
 
 var mia = new Student('Mia');
 mia.__proto__.getName();
 
 Student.prototype === mia.__proto__;
+
+var professor = function (department) {
+    this._department = department;
+}
