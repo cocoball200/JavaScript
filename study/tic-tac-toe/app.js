9aef4b8851b5d3b441d4bebba1dfe85de33396da
Mihyeon Byeon <cocoball200@gmail.com>
2020-11-10 18:01:23 +0900

    reflec on feedback

--- study/tic-tac-toe/app.js
+++ study/tic-tac-toe/app.js
@@ -1,21 +1,5 @@
-//HTML ELEMENTS 
-const statusDiv = document.querySelector('.status');
-const restDiv = document.querySelector('.reset');
-const cellDivs = document.querySelectorAll('.game-cell');
-const gameGridDivs = document.querySelector('.game-grid');
-
-//game constants 
-const xSymbol = 'X';
-const oSymbol = 'O';
-const CLICK_EVENT = 'click';
-
-//game variables
-let player = xSymbol;
-let scoreArray = [];
-let winner = '';
-
-function identifyWinner(scoreArray) {
-    const scoringData = [
+function calculateWinner(squares) {
+    const lines = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -24,45 +8,62 @@ function identifyWinner(scoreArray) {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ]
+    ];
 
-    for (let i = 0; i < scoringData.length; i++) {
-        const [a, b, c] = scoringData[i];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
 
-        if (scoreArray[a] && scoreArray[a] === scoreArray[b] && scoreArray[a] === scoreArray[c]) {
-            return scoreArray[a];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
         }
     }
 
     return null;
 }
 
-function notiWinner(player) {
-    alert(`YEAH! Winner is ${player == xSymbol ? oSymbol : xSymbol}`);
-    resetGame();
+const startButton = document.querySelector('.start__button');
+const restartButton = document.querySelector('.restart__button');
+const bordDiv = document.querySelector('.board');
+const bordElement = document.querySelectorAll('.board__cell');
+const CLICK_EVENT = 'click';
+
+let saveScore = Array(9).fill(null);
+let clickedPartDiv;
+let currentPlayer = 'X';
+
+function handleCellClick(event) {
+    clickedPartDiv = event.target;
+    let index = parseInt(clickedPartDiv.getAttribute('data-index'), 9);
+    if (saveScore[index]) return;
+    saveScore[index] = currentPlayer;
+    clickedPartDiv.textContent = currentPlayer;
+
+    let winner = calculateWinner(saveScore);
+    if (winner) {
+        alert(`${currentPlayer} is winner`);
+        bordDiv.removeEventListener(CLICK_EVENT, handleCellClick);
+        restartButton.addEventListener(CLICK_EVENT, init);
+    }
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
 
-function handleCellScore(clickTarget, targetIndex) {
-    scoreArray[targetIndex] = player;
-    clickTarget.textContent = player;
-    player = player == xSymbol ? oSymbol : xSymbol;
-    statusDiv.textContent = `This is ${player} turn `;
-    winner = identifyWinner(scoreArray);
-    if (!winner) return
-    notiWinner(player);
+function startGame() {
+    bordDiv.style.display = 'grid';
+    startButton.style.display = 'none';
+    restartButton.style.display = 'block';
+    bordDiv.addEventListener(CLICK_EVENT, handleCellClick);
 }
 
-function startGame(event) {
-    var clickTarget = event.target;
-    var targetIndex = parseInt(clickTarget.getAttribute('index'));
-    handleCellScore(clickTarget, targetIndex);
+function init() {
+    bordElement.forEach(elment => elment.textContent = "");
+    restartButton.removeEventListener(CLICK_EVENT, init);
+    saveScore = Array(9).fill(null);
+    bordDiv.style.display = 'none';
+    startButton.style.display = 'block';
+    restartButton.style.display = 'none';
+    startButton.addEventListener(CLICK_EVENT, startGame);
 }
-function resetGame(event) {
-    scoreArray.fill(null, 0);
-    cellDivs.forEach(item => item.textContent = '');
-}
 
-gameGridDivs.addEventListener(CLICK_EVENT, startGame);
-restDiv.addEventListener(CLICK_EVENT, resetGame);
+init();
 
-
+restartButton.addEventListener(CLICK_EVENT, init);
