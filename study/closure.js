ea34bfd0e0ed7d3f7ed74dd7e43b2fc5fa07e901
Mihyeon Byeon <cocoball200@gmail.com>
2020-11-11 17:57:39 +0900

    Update closure.js

--- study/closure.js
+++ study/closure.js
@@ -14,3 +14,85 @@ function say() {
 const func = say();
 
 func();
+
+
+/* 참조 카운트를 0 으로 만드는 방법은*/
+var outer = (function () {
+    var a = 1;
+    var inner = function () {
+        return a++;
+    };
+    return inner;
+})();
+
+console.log(outer())
+console.log(outer())
+outer = null;
+
+(function () {
+    var a = 0;
+    var intervalId = null;
+    var inner = function () {
+        if (++a >= 10) {
+            clearInterval(intervalId);
+            inner = null;
+        }
+        console.log(a);
+    };
+    intervalId = setInterval(inner, 1000);
+})()
+
+    (function () {
+        var count = 0;
+        var button = document.createElement('button');
+        button.innerText = 'click';
+
+        var clickHandler = function () {
+            console.log(++count, 'times clicked');
+            if (count >= 10) {
+                button.removeEventListener('click', clickHandler);
+                clickHandler = null;
+            }
+        };
+        button.addEventListener('click', clickHandler);
+        document.body.appendChild(button);
+    })();
+
+
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    $li.addEventListener('click', function () {
+        alert('your choice is ' + fruit);
+    });
+    $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+
+var alertFruit = function (fruit) {
+    alert('your choice is ' + fruit);
+};
+fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    //$li.addEventListener('click', alertFruit);
+    $li.addEventListener('click', alertFruit.bind(null, fruit)); //null은 this 
+    $ul.appendChild($li);
+});
+document.body.appendChild($ul);
+alertFruit(fruits[1]);
+
+var alertFruitBuilder = function (fruit) {
+    return function () {
+        alert('your choice is ' + fruit);
+    };
+};
+fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    $li.addEventListener('click', alertFruitBuilder(fruit));
+    $ul.appendChild($li);
+});
